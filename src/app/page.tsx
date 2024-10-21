7d2965a0e13b108596df34b10ca6b5720dd9a235
Eugene Kwasny <eugene.kwasny@gmail.com>
2024-10-21 16:57:58 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -277,7 +277,7 @@ export default async function Home() {
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com/EugeneKwasny"
+              href="https://x.com/AlgorithmTrail"
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
             />
